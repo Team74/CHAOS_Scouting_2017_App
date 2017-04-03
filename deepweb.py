d66cf82d6e65a7af3d5fa88dd63d50360fa25fdc
Jacob Gareau <you@example.com  git config --global user.name Your>
2017-04-03 19:11:57 -0400

    updating

--- deepweb.py
+++
@@ -1,8 +0,0 @@
-class scout:
-  print ('sort by...'+ '\n')
-  print ('event, round, team - data.py'+ '\n' )
-  print ('scouter - scouter'+ '\n')
-  print ('team - teamfinder'+ '\n')
-
-if __name__ == "__main__":
-    scout().run()
